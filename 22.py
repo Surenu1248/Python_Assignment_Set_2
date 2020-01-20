2afb3014369f8b4bf947bef008ac72e31094f568
Surendra Katla <31179719+Surenu1248@users.noreply.github.com>
2020-01-20 18:28:57 +0530

    Create 22.py

---
+++ 22.py
@@ -0,0 +1,16 @@
+# Read the value x and y from the user and apply all trigonometric functions on these numbers.
+# Note: Refer the tutorial Trigonometric operation table.
+
+import math
+x = int(input("Enter the x value: "))
+y = int(input("Enter the y value: "))
+
+print(math.sin(x))
+print(math.cos(x))
+print(math.sin(y))
+print(math.cos(y))
+print(math.tan(x))
+print(math.tan(y))
+print(math.hypt(x,y))
+print(math.degrees(x))
+print(math.sin(x))
